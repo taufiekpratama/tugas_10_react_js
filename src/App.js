e778a322315a193d4093d8a95fe42aa5a71a197c
User-niomic <user@niomic.co.id>
2021-08-21 20:43:23 +0700

    Tugas 10 React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import {
+  Grid,
+  Flag,
+  Icon,
+  Input,
+  Image,
+  Header,
+  Divider,
+  Container,
+  Label,
+  Button,
+} from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <br />
+      <Grid container columns={5}>
+        <Grid.Column width="1">
+          <Flag name="id" />
+        </Grid.Column>
+        <Grid.Column width="1">
+          <Icon name="angle left" />
+        </Grid.Column>
+        <Grid.Column width="1">
+          <Icon name="angle right" />
+        </Grid.Column>
+        <Grid.Column width="11">
+          <Input text="text" placeholder="Search.." icon="favorite" fluid />
+        </Grid.Column>
+        <Grid.Column width="2"textAlign="right">
+          <Header>
+            <Image src="https://react.semantic-ui.com/images/avatar/large/patrick.png" circular/>
+            Patrick
+          </Header>
+        </Grid.Column>
+      </Grid>
+      <Divider horizontal>SELAMAT DATANG!!!!</Divider>
+      <Container textAlign="right">
+        <Label color="teal" tag>SPORT</Label>
+      </Container>
+      <br/>
+      <Container textAlign="center">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ducati tengah menunggu sidang Pengadilan Banding FIM tentang komponen
+          aerodinamis. Andai gugatan para rivalnya diterima, akankah Ducati akan
+          melanjutkan ke CAS (Pengadilan Arbitrase Olahraga)? Suzuki, Honda,
+          KTM, dan Aprilia memprotes penggunaan komponen aero di motor
+          Desmosedici Andrea Dovizioso dan Danilo Petrucci dalam balapan pertama
+          MotoGP 2019 di Qatar karena dianggap ilegal. Pada prosesnya, Dovizioso
+          tampil sebagai pemenang usai mengalahkan Marc Marquez. Keempat tim
+          meyakini, komponen itu memiliki fungsi aerodinamis, dan menghasilkan
+          downforce, yang membantu motor melesat di trek. Sementara Ducati
+          bersikukuh komponen itu cuma untuk mendinginkan ban dan sudah
+          disetujui Technical Director sebelum balapan. Pada awalnya, protes
+          keempat tim itu ditolak oleh steward MotoGP sehingga dibawa ke
+          Pengadilan Banding FIM. Sidang itu sendiri akan dilakukan sebelum seri
+          kedua di Argentina digelar pada 29-31 Maret mendatang. Manajer umum
+          Ducati Gigi Dall'Igna berkukuh tidak ada yang ilegal dengan komponen
+          itu. Namun, seandainya pengadilan mengabulkan gugatan tersebut Ducati
+          akan mempertimbangkan melanjutkan kasus ini ke CAS. "Di dalam
+          federasi, derajatnya berakhir di Pengadilan Banding FIM. Anda harus
+          pergi ke CAS, yang adalah pengadilan olahraga tapi di dalam kasus ini
+          Anda keluar dari perimeter FIM," Dall'Igna mengungkapkan kepada
+          MotoGP.com. "Saya bahkan tidak ingin memikirkan sampai titik ini. Kami
+          sangat yakin bahwa kami tidak melanggar regulasi kok, dan yakin kami
+          tidak melihat adanya alasan mengapa pengadilan banding bisa memutuskan
+          secara berbeda."
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button icon labelPosition='left' color='teal'>
+          <Icon name='plus'/>
+          Tambah Tautan Ke List
+          </Button>
+      </Container>
     </div>
   );
 }
